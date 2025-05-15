fb0c5018a8874b1914853495ee349440fd32682b
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-05-16 02:40:15 +0900

    Update 3F_map.py

--- Front/3F_map.py
+++ Front/3F_map.py
@@ -7,23 +7,23 @@ class FloorMap(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("3F Floor Map")
-        self.setGeometry(100, 100, 1400, 800)
+        self.setGeometry(0, 0, 1920, 1080)
         self.setStyleSheet("background-color: black;")
         self.initUI()
 
     def initUI(self):
         # 상단 오른쪽 3F 라벨
         label_3f = QLabel("3F", self)
-        label_3f.setFont(QFont("Arial", 60, QFont.Bold))
+        label_3f.setFont(QFont("Arial", 80, QFont.Bold))
         label_3f.setStyleSheet("color: white;")
-        label_3f.move(1250, 30)
+        label_3f.move(1725, 50)
 
         # 버튼 스타일 공통
         btn_style = """
             QPushButton {
                 background-color: #eeeeee;
                 color: black;
-                font-size: 24px;
+                font-size: 30px;
                 font-weight: bold;
                 border-radius: 10px;
             }
@@ -33,29 +33,29 @@ class FloorMap(QWidget):
         """
 
         # 위 줄 방들 (왼→오)
-        self.make_room("room313", "313",  140, 170, 130, 110, btn_style)
-        self.make_room("room312", "312", 280, 170, 100, 110, btn_style)
-        self.make_room("room311", "311", 390, 170, 100, 110, btn_style)
-        self.make_room("room310", "310", 500, 170, 100, 110, btn_style)
-        self.make_room("room309", "309", 610, 170, 100, 110, btn_style)
-        self.make_room("room308", "308", 720, 170, 100, 110, btn_style)
-        self.make_room("room307", "307", 830, 170, 100, 110, btn_style)
-        self.make_room("room306", "306", 940, 170, 100, 110, btn_style)
-        self.make_room("room305", "305", 1050, 170, 100, 110, btn_style)
-        self.make_room("room304", "304",1160, 170, 100, 110, btn_style)
+        self.make_room("room313", "313",  190, 230, 180, 150, btn_style)
+        self.make_room("room312", "312", 375, 230, 140, 150, btn_style)
+        self.make_room("room311", "311", 520, 230, 140, 150, btn_style)
+        self.make_room("room310", "310", 665, 230, 140, 150, btn_style)
+        self.make_room("room309", "309", 810, 230, 140, 150, btn_style)
+        self.make_room("room308", "308", 955, 230, 140, 150, btn_style)
+        self.make_room("room307", "307", 1100, 230, 140, 150, btn_style)
+        self.make_room("room306", "306", 1245, 230, 140, 150, btn_style)
+        self.make_room("room305", "305", 1390, 230, 140, 150, btn_style)
+        self.make_room("room304", "304", 1535, 230, 140, 150, btn_style)
 
         # 아래 줄 방들
-        self.make_room("room301", "301",  40, 400, 120, 130, btn_style)
-        self.make_room("room302", "302", 170, 400,  90, 130, btn_style)
+        self.make_room("room301", "301",  55,  550, 160, 175, btn_style)
+        self.make_room("room302", "302", 225, 550, 120, 175, btn_style)
 
-        # 중앙 복도 (버튼으로 변경)
+        # 중앙 복도
         hallway = QPushButton("중앙 복도", self)
-        hallway.setGeometry(400, 400, 240, 130)  # 크기 조정
+        hallway.setGeometry(530, 550, 330, 175)
         hallway.setStyleSheet("""
             QPushButton {
                 background-color: #eeeeee;
                 color: black;
-                font-size: 26px;
+                font-size: 32px;
                 font-weight: bold;
                 border-radius: 12px;
             }
@@ -69,8 +69,8 @@ class FloorMap(QWidget):
         hallway.clicked.connect(lambda: self.on_click("중앙 복도"))
 
         # 아래 오른쪽 방
-        self.make_room("room314", "314", 730, 400, 90, 130, btn_style)
-        self.make_room("room303", "303", 830, 400, 200, 130, btn_style)
+        self.make_room("room314", "314", 970, 550, 120, 175, btn_style)
+        self.make_room("room303", "303", 1100, 550, 270, 175, btn_style)
 
     def make_room(self, object_name, text, x, y, w, h, style):
         btn = QPushButton(text, self)
